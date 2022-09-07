47c8970e8a77346913db7a039bafdd67cb006bc3
Calonca <alexlaconca@gmail.com>
2022-09-07 11:28:41 +0200

    Fix terrain and spawn reward blocks

--- Assets/scripts/generateGameobjects.cs
+++ Assets/scripts/generateGameobjects.cs
@@ -9,7 +9,7 @@ using UnityEditor;
 public class generateGameobjects : MonoBehaviour
 {
     public GameObject[] prefab_celestials;
-    public GameObject[] large_celestials;
+    public GameObject[] block_celestials;
     public GameObject chunck;
 
     public float chuncksize = 32;
@@ -23,44 +23,27 @@ public class generateGameobjects : MonoBehaviour
 
     void OnTriggerEnter(Collider collision)
     {
-
-
         if (collision.gameObject.tag.Equals("Player") && !visited)
         {
-
             //Debug.Log("Generating nearby chunks for chuck at position " + transform.position);
-
             spawnChunks();
-
             visited = true;
-
         }
     }
     
     void spawnChunks()
-
     {
-
         Vector3 topPosition = transform.position + new Vector3(0, chuncksize, 0);
-
         Vector3 bottomPosition = transform.position + new Vector3(0, -chuncksize, 0);
-
         Vector3 rigthPosition = transform.position + new Vector3(chuncksize, 0, 0);
-
         Vector3 leftPosition = transform.position + new Vector3(-chuncksize, 0, 0);
-
         
 
         Vector3 topRight = transform.position + new Vector3(chuncksize, chuncksize, 0);
-
         Vector3 topLeft = transform.position + new Vector3(-chuncksize, chuncksize, 0);
-
         Vector3 bottomRight = transform.position + new Vector3(chuncksize, -chuncksize, 0);
-
         Vector3 bottomLeft = transform.position + new Vector3(-chuncksize, -chuncksize, 0);
-
         Vector3[] nearbyChuckPositions = { topPosition, bottomPosition, rigthPosition, leftPosition, topRight, topLeft,bottomRight, bottomLeft};
-
 
 
         float halfSize = (chuncksize +1) / 2;
@@ -68,21 +51,13 @@ public class generateGameobjects : MonoBehaviour
         //Create an array of nearby chucks
 
         Vector3[] collidedPos =
-
             Physics.OverlapBox(transform.position, new Vector3(halfSize,halfSize,halfSize))
-
             .Select(c => c.gameObject)
-
             .Where(o => o.tag.Equals("chuck"))
-
             //.Where(o => o.gameObject.GetComponent<generateGameobjects>().generated==false)
-
             .Select(c => c.transform.position)
-
             .Where(o => o != transform.position)
-
             .Where(pos => nearbyChuckPositions.Contains(pos))
-
             .ToArray();
 
 
@@ -92,27 +67,19 @@ public class generateGameobjects : MonoBehaviour
         //print collidedGameObjects
 
         foreach (Vector3 spawnPos in spawnPoss)
-
         {
 
                 //Debug.Log("Will spawn chuck at"+ spawnPos);
 
                 GameObject newChunk = Instantiate(chunck, spawnPos, Quaternion.identity); //generate nearby missing chunks
-
                 newChunk.transform.parent = transform.parent;
-
                 newChunk.gameObject.name = "chuck" + spawnPos.ToString();
 
                 //Instance is creating a copy so you need to delete the children on the old object
-
                 foreach (Transform child in newChunk.transform)
-
                 {
-
                     GameObject.Destroy(child.gameObject);
-
                 }
-
                 //newChunk.GetComponent<generateGameobjects>().spawnTerrain();
 
         }
@@ -120,11 +87,8 @@ public class generateGameobjects : MonoBehaviour
     }
 
     void spawnTerrain()
-
     {
-
         spawnobj_start();
-
     }
 
 
@@ -134,47 +98,59 @@ public class generateGameobjects : MonoBehaviour
     void Update()
     {
         if (GameObject.Find("Player").transform.position.y - chuncksize*2 >= transform.position.y)
-
         {
-
             Destroy(gameObject);
-
         }
     }
 
-    void spawnobj_start(){    //random generation planets of 3 diff sizes  initially
-        int x = Random.Range(20, 25); // number of objects to be generated initially
+    void spawnobj_start(){ 
+        
+        //random generation planets of 3 diff sizes  initially
+        int xObj = Random.Range(20, 25); // number of objects to be generated initially
+        int xLarge = Random.Range(0, 2); // number of reward blocks to be generated initially
         float initial_pos_y, initial_pos_x; // Z axis is fixed, defining other two coordinates, i.e, X and Y.
         int i = 0; // variable for counter to generate objects
-
         
-        while (i<x)
+        //Spawn reward blocks
+        while (i < xLarge)
+        {
+            float halfSize = chuncksize / 2;
+            initial_pos_x = Random.Range(-halfSize, halfSize) + transform.position.x; 
+            initial_pos_y = Random.Range(-halfSize, halfSize) + transform.position.y; 
+            bool isColliding = Physics.OverlapSphere(new Vector3(initial_pos_x, initial_pos_y, transform.position.z), 5)
+                .Where(c => !c.gameObject.tag.Equals("chuck"))
+                .ToArray().Length > 0;
+
+            if (!isColliding) 
+            {
+                GameObject toSpawn = block_celestials[Random.Range(0, block_celestials.Length)];
+                GameObject terrainObj = Instantiate(toSpawn, new Vector3(initial_pos_x, initial_pos_y, 0), Quaternion.identity); //generate the planet of any of 3 diff size on the X and Y pos if above confition is false
+                terrainObj.transform.parent = transform;
+
+            }
+            i++;
+        }
+        
+        i = 0;
+        //Spawn object
+        while (i<xObj)
         {
             float halfSize = chuncksize / 2;
             initial_pos_x = Random.Range(-halfSize, halfSize) +transform.position.x; //range of X Coordinate to generate the planet object
             initial_pos_y = Random.Range(-halfSize, halfSize) +transform.position.y; //range of Y Coordinate to generate the planet object.Initially generated till 30 units on Y axis.
 
-
-
             
-
             /*x = 1;
             initial_pos_x = transform.position.x; //range of X Coordinate to generate the planet object
             initial_pos_y = transform.position.y; //range of Y Coordinate to generate the planet object.Initially generated till 30 units on Y axis.
 
             */
-
-
-
+            
             bool isColliding = Physics.OverlapSphere(new Vector3(initial_pos_x, initial_pos_y, transform.position.z),2)
-
                 .Where(c => !c.gameObject.tag.Equals("chuck"))
-
                 .ToArray().Length > 0;
 
-
-
-            if (!isColliding) // to check if any other sphere object(planet object ) generated within given pos and and radius
+            if (!isColliding) 
             {
                 GameObject toSpawn = prefab_celestials[Random.Range(0, prefab_celestials.Length)];
                 //toSpawn = prefab_celestials[Random.Range(4, 6)];
